502bdea4697e50704e21ffd407637c2f8dddf9da
Nikhil <nikhiltiware@gmail.com>
2015-11-10 16:34:05 -0500

    Group home title, navigation added Android support libs updated

--- GradeUP/app/src/main/java/com/ufl/gradeup/GroupHomeActivity.java
+++ GradeUP/app/src/main/java/com/ufl/gradeup/GroupHomeActivity.java
@@ -1,13 +1,14 @@
 package com.ufl.gradeup;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class GroupHomeActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,17 @@ public class GroupHomeActivity extends AppCompatActivity {
         if (extras != null) {
             String value = extras.getString("new_variable_name");
         }
+
+        Toolbar groupProfileToolbar = (Toolbar) findViewById(R.id.groupHomeToolbar);
+        setSupportActionBar(groupProfileToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_groupPic);
+        collapsingToolbar.setTitle("Add Group Name Here");
+
+        NavigationDrawerFragment navDrawerFrag = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.nav_drawer_fragment);
+        navDrawerFrag.setUp(R.id.nav_drawer_fragment, (DrawerLayout) findViewById(R.id.group_drawer_layout), groupProfileToolbar);
     }
 
     @Override
